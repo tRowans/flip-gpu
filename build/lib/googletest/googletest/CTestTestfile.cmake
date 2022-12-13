541980172c032d42601dde8f026d59fb51d6fc77
tRowans <tomrs@live.co.uk>
2022-12-13 13:49:52 +0900

    final private commit (1)

--- build/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/NemotoU/tom/flip_gpu/lib/googletest/googletest
-# Build directory: /work/NemotoU/tom/flip_gpu/build/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
